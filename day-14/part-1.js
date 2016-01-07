e71edb4ba570ddb5f567c2bd0f33761abfc3e8a0
MaxArt2501 <maxart.x@gmail.com>
2016-01-07 01:02:40 +0100

    Day 14 explanations

--- day-14/part-1.js
+++ day-14/part-1.js
@@ -1,4 +1,3 @@
-// In the end, it's all about computing the distance that each reindeer has flown after 2503 seconds.
 var RACE_DURATION = 2503;
 
 var distances = input.slice(0, -1).split("\n").map(line => {
@@ -13,4 +12,9 @@ var distances = input.slice(0, -1).split("\n").map(line => {
 
 Math.max(...distances);
 
-// Explanation: ... later.
+// Explanation: To compute the distance each reindeer has flown, we need to know the number of seconds they
+// fly during the race time. Every reindeer have cycles of flight and rest, with a period that's the sum of
+// their flight and rest times. For each cycles, each reindeer has flown for their respecitve flight periods.
+// So each reindeer flies for their flight periods, multiplied by the number of complete cycles performed,
+// plus a remainder of seconds that's maxed by their flight periods. Multiply this by their flight speed,
+// and you'll get the distance flown by each reindeer.
--- day-14/part-2.js
+++ day-14/part-2.js
@@ -2,6 +2,8 @@
 // actually "race".
 var RACE_DURATION = 2503;
 
+// We collect the data about the reindeers here, including their cycle periods. We don't actually need to
+// know their resting times. In these objects, we'll also store the distance flown and the points gained.
 var reindeers = input.slice(0, -1).split("\n").map(line => {
     let [ , speed, flying, resting ]
             = line.match(/^\w+ can fly (\d+) km\/s for (\d+) seconds, but then must rest for (\d+) seconds\.$/);
@@ -15,17 +17,24 @@ var reindeers = input.slice(0, -1).split("\n").map(line => {
     };
 });
 
+// Determines if a reindeer is flying at a given time (in seconds). We only need to check if the time is in
+// the middle of the reindeer's flight time, which happens at the beginning of its cycle period.
 var isFlying = (reindeer, time) => time % reindeer.period < reindeer.flying;
 
+// We will simulate the whole race at every second, everything other attempt whould be over-complicated for
+// just 2503 iterations.
 for (var time = 0; time < RACE_DURATION; time++) {
+    // Computing the distance each reindeer has flown so far.
     let distances = reindeers.map(reindeer => {
         if (isFlying(reindeer, time))
             reindeer.distance += reindeer.speed;
 
         return reindeer.distance;
     });
+    // The leading reindeer(s) has flown this much.
     var front = Math.max(...distances);
 
+    // If a reindeer is leading, it gets a point.
     reindeers.forEach(reindeer => {
         if (reindeer.distance === front)
             reindeer.points++;
@@ -33,5 +42,3 @@ for (var time = 0; time < RACE_DURATION; time++) {
 }
 
 Math.max(...reindeers.map(reindeer => reindeer.points));
-
-// Explanation: ... later.
